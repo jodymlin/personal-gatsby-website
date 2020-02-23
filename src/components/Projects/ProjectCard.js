61e35f1de35c0fa15f74fba1b24c260bafac3e02
Jody Lin <jody.m.lin@gmail.com>
2020-02-23 13:57:44 -0800

    creating CardMedia

--- src/components/Projects/ProjectCard.js
+++ src/components/Projects/ProjectCard.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { withStyles } from '@material-ui/core/styles'
+import { withStyles, makeStyles } from '@material-ui/core/styles'
 import PropTypes from 'prop-types'
 
 import { Typography } from '@material-ui/core'
@@ -11,20 +11,40 @@ const styles = theme => ({
         overflowY: 'hidden',
         margin: theme.spacing (1, 0)
     },
-    media: {
-        width: '100%'
-    },
     description: {
     }
 });
 
+const useCardStyles = makeStyles({
+    media: {
+        width: '100%'
+    }
+});
+
+function CardMedia({ data }) {
+    const { media, mediaType } = data;
+    const classes = useCardStyles();
+    if (mediaType === 'img') {
+        return <img src={media} className={classes.media}/>
+    }
+    else if (mediaType === 'mp4') {
+        return <video src={media} height='100%' controls muted></video>
+    }
+    else if (mediaType === 'youtube') {
+        return <iframe src={media} height='100%' frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    }
+    else {
+        return <Typography>Unknown media type</Typography>
+    }
+}
+
 function ProjectCard({ classes, data }) {
-    const { title, media, description} = data;
+    const { title, description} = data;
     return(
         <div>
             <Typography variant='h6'>{title}</Typography>
             <div className={classes.mediaWrapper}>
-                <img src={media} className={classes.media}/>
+                <CardMedia data={data}/>
             </div>
             <div className={classes.description}>
                 <Typography>{ description }</Typography>
--- src/components/Projects/ProjectPreview.js
+++ src/components/Projects/ProjectPreview.js
@@ -5,11 +5,14 @@ import PropTypes from 'prop-types'
 import ProjectCard from './ProjectCard'
 import placeholder from '../../media/placeholder.jpg'
 import hackWebsite from '../../media/hack_website.png'
+import snakeDemo from '../../media/snake_demo.mp4'
+import myCarDemo from '../../media/mycar_demo.mp4'
 
 const projects = [
     {
         title : 'Project Title',
         media: placeholder,
+        mediaType: 'img',
         description: `the quick brown fox jumped over the lazy dog 
                         the quick brown fox jumped over the lazy dog. 
                         the quick brown fox jumped over the lazy dog. 
@@ -19,6 +22,7 @@ const projects = [
     {
         title : 'Another Project',
         media: placeholder,
+        mediaType: 'img',
         description: `the quick brown fox jumped over the lazy dog 
                         the quick brown fox jumped over the lazy dog. 
                         the quick brown fox jumped over the lazy dog. 
@@ -28,6 +32,7 @@ const projects = [
     {
         title: 'UCLA ACM Hack Website',
         media: hackWebsite,
+        mediaType: 'img',
         description: `UCLA ACM Hack's website built 
         with React.js and Gatsby. 
         Check it out for updates on Hack's events and 
@@ -35,7 +40,8 @@ const projects = [
     },
     {
         title: 'Sensortile Snake',
-        media: hackWebsite,
+        media: snakeDemo,
+        mediaType: 'mp4',
         description: `Final Project for Engr 96C. A take on the
         classic Snake game that uses the accelerometer
         in a SensorTile IoT module to determine player
@@ -43,7 +49,8 @@ const projects = [
     },
     {
         title: 'MyCar App',
-        media: hackWebsite,
+        media: myCarDemo,
+        mediaType: 'mp4',
         description: `An app created with React Natvie at LAHacks 
         that provides a convenient place containing all information
         about your car. Implemented the Google Maps feature that 
@@ -51,7 +58,8 @@ const projects = [
     },
     {
         title: 'Bows and Arrows',
-        media: hackWebsite,
+        media: 'https://www.youtube.com/embed/yYe7Sl0CTTw',
+        mediaType: 'youtube',
         description: `Throwback to 11-year-old me living out my dreams 
         of becoming Katniss Everdeen after making my own bows and arrows :)`
     },
