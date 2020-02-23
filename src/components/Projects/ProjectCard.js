b02c3b093939129929a51ed84403ad67379e4481
Jody Lin <jody.m.lin@gmail.com>
2020-02-22 16:46:43 -0800

    finished project preview template

--- src/components/Projects/ProjectCard.js
+++ src/components/Projects/ProjectCard.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import { withStyles } from '@material-ui/core/styles'
+import PropTypes from 'prop-types'
+
 import { Typography } from '@material-ui/core'
-import placeholder from '../../media/placeholder.jpg'
 
 const styles = () => ({
     mediaWrapper: {
         width: '100%',
-        height: '250px',
+        maxHeight: '250px',
         overflowY: 'hidden'
     },
     media: {
@@ -14,24 +15,26 @@ const styles = () => ({
     }
 });
 
-function ProjectCard({ classes }) {
+function ProjectCard({ classes, data }) {
+    const { title, media, description} = data;
     return(
         <div>
-            <Typography variant='h6'>Project Title</Typography>
+            <Typography variant='h6'>{title}</Typography>
             <div className={classes.mediaWrapper}>
-                <img src={placeholder} className={classes.media}/>
+                <img src={media} className={classes.media}/>
             </div>
             <div>
-                Project Description: slkdjfa
-                the quick brown fox jumps over the lazy dog. 
-                the quick brown fox jumps over the lazy dog. 
-                the quick brown fox jumps over the lazy dog. 
-                the quick brown fox jumps over the lazy dog. 
-                the quick brown fox jumps over the lazy dog. 
+                { description }
             </div>
         </div>
         
     )
+}
+
+
+ProjectCard.propTypes = {
+    classes: PropTypes.object.isRequired,
+    data: PropTypes.object.isRequired
 }
 
 export default withStyles(styles)(ProjectCard);
--- src/components/Projects/ProjectPreview.js
+++ src/components/Projects/ProjectPreview.js
@@ -1,14 +1,45 @@
 import React from 'react'
-import { Typography } from '@material-ui/core'
+import { Grid } from '@material-ui/core'
+import PropTypes from 'prop-types'
 
 import ProjectCard from './ProjectCard'
+import placeholder from '../../media/placeholder.jpg'
 
-function ProjectPreview() {
-    return (<>
-        <Typography>Projects go here</Typography>
-        <ProjectCard/>
-        </>
-    )
+const projects = [
+    {
+        title : 'Project Title',
+        media: placeholder,
+        description: `the quick brown fox jumped over the lazy dog 
+                        the quick brown fox jumped over the lazy dog. 
+                        the quick brown fox jumped over the lazy dog. 
+                        the quick brown fox jumped over the lazy dog.
+                        the quick brown fox jumped over the lazy dog.`
+    },
+    {
+        title : 'Another Project',
+        media: placeholder,
+        description: `the quick brown fox jumped over the lazy dog 
+                        the quick brown fox jumped over the lazy dog. 
+                        the quick brown fox jumped over the lazy dog. 
+                        the quick brown fox jumped over the lazy dog.
+                        the quick brown fox jumped over the lazy dog.`
+    }
+];
+
+function ProjectPreview({ classes }) {
+
+    return <Grid container spacing={5}>
+            {projects.map( o => 
+                <Grid item xs={12} sm={6} md={6} id={o.title}>
+                    <ProjectCard data={o} />
+                </Grid>
+            )}
+        </Grid>
+        
+}
+
+ProjectPreview.propTypes = {
+    classes: PropTypes.object.isRequired
 }
 
 export default ProjectPreview;
