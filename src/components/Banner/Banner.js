57370d189b9177b92c6671a28accff1b5dbad9bb
Jody Lin <jody.m.lin@gmail.com>
2020-03-27 11:35:25 -0700

    optimized banner background implementation

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -7,23 +7,32 @@ import PhoneAndroidIcon from '@material-ui/icons/PhoneAndroid'
 import MailOutlineIcon from '@material-ui/icons/MailOutline'
 
 import taiwan from '../../media/taiwan.jpg'
+import japanwalk from '../../media/japanwalk.jpg'
 
 const styles = theme => ({
     title: {
-        padding: theme.spacing(2)
+        padding: theme.spacing(2),
+        [theme.breakpoints.down('xs')]: {
+            padding: theme.spacing(1),
+            margin: theme.spacing(1, 0)
+        }
     },
     background: {
+        position: 'relative',
+        height: '200px',
+        overflow: 'hidden',
         display: 'flex',
         alignItems: 'center',
-        height: '200px',
-        backgroundImage: `url(${taiwan})`,
-        backgroundPosition: 'center',
-        overflow: 'hidden'
+    },
+    backgroundImage: {
+        position: 'absolute',
+        width: '100%'
     },
     overlay: {
         width: '100%',
         padding: theme.spacing(1.5, 0),
-        backgroundColor: `rgb(255, 255, 255, 0.7)`
+        backgroundColor: `rgb(255, 255, 255, 0.7)`,
+        position: 'absolute'
     },
     contactItems: {
         padding: theme.spacing(1.5, 0)
@@ -39,39 +48,41 @@ function Banner({classes, ...props}) {
     const ContactItem = ({children}) => <Grid item sm={3} xs={12} className={classes.item}>
             {children}
         </Grid>
-    return(
-        <section className={classes.background} {...props}>
+    return( <>
+        <div className={classes.background}>
+            <img src={japanwalk} className={classes.backgroundImage}/>
             <div className={classes.overlay}>
-                <Typography variant='h3' component='h1' align='center' className={classes.title}>
-                    Jody Lin
-                </Typography>
-                <Grid container className={classes.contactItems}>
-                    <ContactItem>
-                        <MailOutlineIcon />
-                        <Typography align='center'> jody.m.lin@gmail.com </Typography>
-                    </ContactItem>
-                    <ContactItem>
-                        <PhoneAndroidIcon />
-                        <Typography align='center'> (949) 377-5165 </Typography>
-                    </ContactItem>
-                    <ContactItem>
-                        <Link href='https://github.com/jodymlin' rel="noopener noreferrer" target="_blank"
+            <Typography variant='h3' component='h1' align='center' className={classes.title}>
+                Jody Lin
+            </Typography>
+            <Grid container className={classes.contactItems}>
+                <ContactItem>
+                    <MailOutlineIcon />
+                    <Typography align='center'> jody.m.lin@gmail.com </Typography>
+                </ContactItem>
+                <ContactItem>
+                    <PhoneAndroidIcon />
+                    <Typography align='center'> (949) 377-5165 </Typography>
+                </ContactItem>
+                <ContactItem>
+                    <Link href='https://github.com/jodymlin' rel="noopener noreferrer" target="_blank"
+                    color='textPrimary' className={classes.item}>
+                        <GitHubIcon />
+                        <Typography align='center'>github</Typography>
+                    </Link>
+                </ContactItem>
+                <ContactItem>
+                    <Link href='https://www.linkedin.com/in/jody-lin-a5b390172/' 
+                        rel="noopener noreferrer" target="_blank"
                         color='textPrimary' className={classes.item}>
-                            <GitHubIcon />
-                            <Typography align='center'>github</Typography>
-                        </Link>
-                    </ContactItem>
-                    <ContactItem>
-                        <Link href='https://www.linkedin.com/in/jody-lin-a5b390172/' 
-                            rel="noopener noreferrer" target="_blank"
-                            color='textPrimary' className={classes.item}>
-                            <LinkedInIcon />
-                            <Typography align='center'>linkedin</Typography>
-                        </Link>
-                    </ContactItem>
-                </Grid>
-            </div>
-        </section>
+                        <LinkedInIcon />
+                        <Typography align='center'>linkedin</Typography>
+                    </Link>
+                </ContactItem>
+            </Grid>
+        </div>
+        </div>
+        </>
     );
 }
 
