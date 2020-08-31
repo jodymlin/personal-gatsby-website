cd3c75d1c77dda65e1427c9f65ff0850b7461d0e
Jody Lin <jody.m.lin@gmail.com>
2020-08-30 21:44:41 -0700

    updated resume link for mobile

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -6,9 +6,9 @@ function Resume() {
     return (
         <object data={resume} type="application/pdf" width='100%' height='500px'>
             <Typography>
-                Your browser isn't supporting embedded pdf files.
+                Your browser doesn't support embedded pdf files.
                 You can download the file
-                <Link href="https://drive.google.com/file/d/1s3AMvX_0_SiQcQO86blMyJVuth-7CK0t/view?usp=sharing">
+                <Link href="https://drive.google.com/file/d/1TSGK8MtC0tzjvJM_77i1w0hLuCQ7Co-5/view?usp=sharing">
                     &nbsp;here
                 </Link>.
             </Typography>
